8a5f62e62f9366cea16a6d8b8ea960caaa38b0e1
Igormandello <igormandello@gmail.com>
2020-04-10 16:34:35 -0300

    Adds texts to type description card

--- src/components/home/TypeDescriptionCard.vue
+++ src/components/home/TypeDescriptionCard.vue
@@ -17,19 +17,19 @@ import { mapGetters } from 'vuex'
 const contentByType = {
   learn: {
     title: 'Aprender',
-    description: 'Descrição dessa categoria, com exemplos lorem ipsum oadsissad'
+    description: 'Aqui você vai encontrar shows, lives, festivais e congressos. Tudo online, hein?'
   },
   participate: {
     title: 'Participar',
-    description: 'Descrição dessa categoria, com exemplos lorem ipsum oadsissad'
+    description: 'Cursos online, palestra, mentoras. Para todos os interesses, idades e níveis de especialidade, e o que é melhor: grátis!'
   },
   do: {
     title: 'Fazer',
-    description: 'Descrição dessa categoria, com exemplos lorem ipsum oadsissad'
+    description: 'Atividades para fazer com seus filhos, streaming e aplicativos liberados de graça. O que tá esperando?'
   },
   share: {
     title: 'Compartilhar',
-    description: 'Descrição dessa categoria, com exemplos lorem ipsum oadsissad'
+    description: 'Informações sobre a pandemia, podcasts, listas, perfis do Instagram e canais do YouTube interessantes'
   }
 }
 
@@ -52,6 +52,8 @@ export default {
 
 <style lang="scss">
 .type-description-card {
+  display: flex;
+  flex-direction: column;
   font-size: 14px;
 
   & > h1 {
@@ -69,6 +71,7 @@ export default {
     font-size: 1em;
     line-height: 1.35;
     padding: 8px;
+    flex-grow: 1;
 
     & > h2 {
       text-transform: uppercase;
